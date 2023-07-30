bb9df2952d291f485f9a72a81f5051ccdc60fe30
tuanta <tuantvbg@gmail.com>
2023-07-31 02:29:50 +0700

    first commit

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
+use App\Http\Resources\UserResource;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return UserResource::collection(User::query()->orderBy('id', 'desc')->paginate(10));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreUserRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreUserRequest $request)
+    {
+        $date = $request->validated();
+        $date['password'] = bcrypt($date['password']);
+        $user = User::create($date);
+        return response(new UserResource($user), 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        return new UserResource($user);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateUserRequest  $request
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateUserRequest $request, User $user)
+    {
+        $date = $request->validated();
+        if(isset($date['password'])){
+            $date['password'] = bcrypt($date['password']);
+        }
+        $user->update($date);
+
+        return new UserResource($user);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+
+        return response("", 204);
+    }
+}
---
+++ react/src/components/DefaultLayout.jsx
@@ -0,0 +1,58 @@
+import { useEffect } from "react";
+import { Outlet, Navigate, Link } from "react-router-dom";
+import { useStateContext } from "../contexts/ContextProvider";
+import axiosClient from "../views/axios-client";
+
+
+export default function DefaultLayout() {
+    const { user, token, notification, setUser, setToken } = useStateContext()
+
+    if (!token) {
+        return <Navigate to="/login" />
+    }
+
+    const onLogout = (e) => {
+        e.preventDefault();
+
+        axiosClient.post('/logout')
+        .then(() => {
+            setUser({})
+            setToken(null)
+        })
+    };
+
+    useEffect(() => {
+        axiosClient.get('/user')
+          .then(({data}) => {
+             setUser(data)
+          })
+      }, [])
+
+    return (
+        <div id="defaultLayout">
+            <aside>
+                <Link to="/dashboard">Dashboard</Link>
+                <Link to="/users">Users</Link>
+            </aside>
+            <div className="content">
+                <header>
+                    <div>
+                        Header
+                    </div>
+                    <div>
+                        {user.name} &nbsp;
+                        <a onClick={onLogout} className="btn-logout" href="#">Logout</a>
+                    </div>
+                </header>
+                <main>
+                    <Outlet />
+                </main>
+                {notification &&
+                    <div className="notification">
+                        {notification}
+                    </div>
+                }
+            </div>
+        </div>
+    )
+}
